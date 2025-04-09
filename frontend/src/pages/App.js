4fc90130e54771abb266d35ce13b53b76a1b144e
womss <tjdals3759@gmail.com>
2025-04-09 14:35:01 +0900

    개인 테스트용 프로젝트 업로드

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
+// src/pages/App.js
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import SorticHeader from '../components/Header/Header';
 import LandingPage from '../components/LandingPage/LandingPage';
 import SorterPage from '../components/SorterPage/components/SorterPage';
+import LoginPage from '../components/LoginPage/LoginPage';
 
 const App = () => {
+    // 전역 테마 초기화
+    useEffect(() => {
+        const saved = localStorage.getItem('theme');
+        document.documentElement.setAttribute('data-theme', saved || 'light');
+    }, []);
+
     return (
         <>
-            <SorticHeader />
-            <Routes>
-                <Route path="/" element={<LandingPage />} />
-                <Route path="/sorter" element={<SorterPage />} />
-            </Routes>
+        <SorticHeader />
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sorter" element={<SorterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+        </Routes>
         </>
     );
 };
